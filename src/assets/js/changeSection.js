ff815352f9a5ab801388c3796f6cc91a312c00b0
Otávio <12201000759@muz.ifsuldeminas.edu.br>
2024-09-26 13:24:23 -0300

    style: changing css variables

---
+++ src/assets/js/changeSection.js
@@ -0,0 +1,12 @@
+const generateBtn = document.querySelector('#generate-btn');
+const validateBtn = document.querySelector('#validate-btn');
+
+function changeVisibility() {
+
+    
+}
+
+if (generateBtn && validateBtn) {
+
+
+}
