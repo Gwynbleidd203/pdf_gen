25cbd368501f9fec8c5d241f54359c4f6ba43814
Otávio <12201000759@muz.ifsuldeminas.edu.br>
2024-09-30 13:21:02 -0300

    chore: inteface creation

--- src/assets/js/changeSection.js
+++ src/assets/js/changeSection.js
@@ -1,12 +1,56 @@
-const generateBtn = document.querySelector('#generate-btn');
-const validateBtn = document.querySelector('#validate-btn');
+const controlObj = {
 
-function changeVisibility() {
+    validate: {
 
-    
+        section: document.querySelector('#validate-section'),
+        btn: document.querySelector('#validate-btn'),
+        isVisible: false,
+        handleClick: function() {
+
+            this.changeVisibility()
+        },
+        changeVisibility: function() {
+
+            this.isVisible = this.section.dataset.showing === "true"
+            this.section.dataset.showing = !this.isVisible
+        }
+    },
+
+    generate: {
+
+        section: document.querySelector('#generate-section'),
+        btn: document.querySelector('#generate-btn'),
+        isVisible: true,
+        handleClick: function() {
+
+            this.changeVisibility()
+        },
+        changeVisibility: function() {
+
+            this.isVisible = this.section.dataset.showing === "true"
+            this.section.dataset.setAttribute.dataShowing = !this.isVisible
+        }
+    },
+
+    init: function() {
+
+        this.validate.isVisible = this.validate.section.dataset.showing === "true";
+        this.generate.isVisible = this.generate.section.dataset.showing === "true";
+
+        if (this.validate.btn && this.generate.btn) {
+            this.validate.btn.addEventListener('click', this.validate.handleClick.bind(this.validate));
+            this.generate.btn.addEventListener('click', this.generate.handleClick.bind(this.generate));
+        }
+    }
 }
 
-if (generateBtn && validateBtn) {
+/**
+ * 
+ * @param {Event} e 
+ */
 
 
+if (controlObj) {
+
+    console.log(controlObj)
 }
